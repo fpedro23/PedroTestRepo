3e1c43d0f35f59298040d7516f4781a21f745b21
Pedro Contreras <fpedro23@gmail.com>
2014-08-28 21:40:17 -0500

    Creation of model branch

---
+++ Pedro/Person.h
@@ -0,0 +1,14 @@
+//
+//  Person.h
+//  Pedro
+//
+//  Created by Pedro Contreras Nava on 28/08/14.
+//  Copyright (c) 2014 Pedro Contreras Nava. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+@property NSString *name;
+
+@end
